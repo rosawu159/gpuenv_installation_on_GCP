687d76adeeec073e7ebf1ee860afb7bac8d72728
Rosa Wu <u10050070@gmail.com>
2019-02-13 04:08:48 +0800

    Create hi.sh

---
+++ hi.sh
@@ -0,0 +1,5 @@
+sudo apt-get update
+sudo apt-get install libglu1-mesa libxi-dev libxmu-dev -y
+sudo apt-get --yes install build-essential
+sudo apt-get install python-pip python-dev -y
+sudo apt-get install python-numpy python-scipy -y
